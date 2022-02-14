23a54ab9ce7905e581ceeb81bd942d3d62617906
melihyuksel <melihyuxel@gmail.com>
2022-02-14 14:37:49 +0300

    fix

--- lib/logger.module.ts
+++ lib/logger.module.ts
@@ -1,8 +1,12 @@
-import { DynamicModule, Module } from '@nestjs/common';
+import { DynamicModule, Module, Provider } from '@nestjs/common';
 import { WinstonModule } from 'nest-winston';
 import { LoggerService } from './logger.service';
-import { LokiLoggerAsyncOptions, LokiLoggerOptions } from './logger.module.interface';
+import { v4 as uuid } from 'uuid';
+import { LoggerOptionsFactory, LokiLoggerAsyncOptions, LokiLoggerOptions } from './logger.module.interface';
 const LokiTransport = require('winston-loki');
+export const LOKI_MODULE_ID = 'LOKU_MODULE_ID';
+export const LOKI_MODULE_OPTIONS = 'MULTER_MODULE_OPTIONS';
+export const randomStringGenerator = () => uuid();
 
 @Module({})
 export class LokiLogger {
@@ -23,6 +27,7 @@ export class LokiLogger {
       ],
     };
   }
+  /*
   static registerAsync(options: LokiLoggerAsyncOptions): DynamicModule {
     return {
       module: LokiLogger,
@@ -38,6 +43,53 @@ export class LokiLogger {
           ],
         }),
       ],
+    };
+  }
+  */
+  static registerAsync(options: LokiLoggerAsyncOptions): DynamicModule {
+    return {
+      module: LokiLogger,
+      imports: options.imports,
+      providers: [
+        ...this.createAsyncProviders(options),
+        {
+          provide: LOKI_MODULE_ID,
+          useValue: randomStringGenerator(),
+        },
+      ],
+      exports: [LOKI_MODULE_OPTIONS],
+    };
+  }
+  private static createAsyncProviders(
+    options: LokiLoggerAsyncOptions,
+  ): Provider[] {
+    if (options.useExisting || options.useFactory) {
+      return [this.createAsyncOptionsProvider(options)];
+    }
+    return [
+      this.createAsyncOptionsProvider(options),
+      {
+        provide: options.useClass,
+        useClass: options.useClass,
+      },
+    ];
+  }
+
+  private static createAsyncOptionsProvider(
+    options: LokiLoggerAsyncOptions,
+  ): Provider {
+    if (options.useFactory) {
+      return {
+        provide: LOKI_MODULE_OPTIONS,
+        useFactory: options.useFactory,
+        inject: options.inject || [],
+      };
+    }
+    return {
+      provide: LOKI_MODULE_OPTIONS,
+      useFactory: async (optionsFactory: LoggerOptionsFactory) =>
+        optionsFactory.createLoggerOptions(),
+      inject: [options.useExisting || options.useClass],
     };
   }
 }
